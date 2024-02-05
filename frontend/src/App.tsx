845e8d010fac6adc11565f958adcb8bef24bb21b
jaftan21 <mrmirofficial@gmail.com>
2024-02-05 23:23:51 +0600

    router initialized

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
-import DigiAnaClock from "./projects/digiana-clock";
+import { lazy } from "react";
+import { Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const Projects = lazy(() => import("src/projects"));
 
 function App() {
   return (
-    <>
-      <div className="mt-2">
-        <DigiAnaClock />
-      </div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/projects" />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/src/projects/index.tsx
@@ -0,0 +1,12 @@
+import DigiAnaClock from "./digiana-clock";
+
+export default function index() {
+  return (
+    <div>
+      <h1>Projects</h1>
+      <div className="mt-2">
+        <DigiAnaClock />
+      </div>
+    </div>
+  );
+}
